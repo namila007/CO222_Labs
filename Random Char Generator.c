66d5626b9356d07152346f76005329f5e8cd88fd
namila007 <namila007@gmail.com>
2017-01-28 14:54:16 +0530

    shorted and added comments

--- Random Char Generator.c
+++ Random Char Generator.c
@@ -2,29 +2,18 @@
 #include <stdlib.h>
 
 int main(){
-
- int i, n,t;
-   time_t e;
+int i, n,t;
+time_t e;  
    char set [8]={'P','S','C','R','L','K','D','f'};
    /* Intializes random number generator */
    srand((unsigned) time(&e));
 
    /* random number generate and assign to char*/
-   for( i = 0 ; i < 40 ; i++ ) 
-  
-   {
-     n= rand() % 7;
+   for( i = 0 ; i < 40 ; i++ ) {
+     n= rand() % 7;   //assigning random integer between 0-7
      t= rand() % 7;
-  
-      
-     printf("%c %c\n",set[n],set[t] );
-    
-
-   }
+    printf("%c %c\n",set[n],set[t] );   //printing
+    }
    
    return(0);
-
-
-
-
 }
