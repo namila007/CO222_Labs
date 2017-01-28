891d18f3df583f2c3643256d19c4aba03f0caa95
namila007 <namila007@gmail.com>
2017-01-28 14:37:19 +0530

    updated the code

--- Random Char Generator.c
+++ Random Char Generator.c
@@ -5,23 +5,21 @@ int main(){
 
  int i, n,t;
    time_t e;
-   
    n = 5;
    char set [8]={'P','S','C','R','L','K','D','f'};
    /* Intializes random number generator */
    srand((unsigned) time(&e));
 
-   /* Print 5 random numbers from 0 to 49 */
+   /* random number generate and assign to char*/
    for( i = 0 ; i < 40 ; i++ ) 
   
    {
-   	 n=	rand() % 7;
-   	 t=	rand() % 7;
-   	 //printf("%d %d\n",n,t);
-   	  
-   	 printf("echo '%c %c'| ./a.out\n",set[n],set[t] );
-     // printf("%c",set[n] );
-       //printf(" %c\n",set[t] );
+     n= rand() % 7;
+     t= rand() % 7;
+  
+      
+     printf("echo '%c %c'| ./a.out\n",set[n],set[t] );
+    
 
    }
    
