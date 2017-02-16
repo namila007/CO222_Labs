cefd8398a33e5d99422bf15eca3d38a917520ff7
Namila Bandara <namila007@gmail.com>
2017-02-16 08:46:10 +0530

    adding fucntions

--- ASCII drawer/asciidraw.c
+++ ASCII drawer/asciidraw.c
@@ -12,22 +12,28 @@ Ascii Draw
 #define BRIGHT 		1
 
 #define BLACK 		0
-#define RED		1
+#define RED			1
 #define GREEN		2
 #define YELLOW		3
 #define BLUE		4
 #define MAGENTA		5
 #define CYAN		6
 #define	WHITE		7
-//dd
+#define argsize
+#define argsize_max 8
+#define box_max		8
+
 char *ck="checker";                      //declaring variables
 char *dn="donut";
-char *colourval[]={"black","red","green","yellow","blue","magenta","cyan","white"};
-int x,y,xbox,ybox,xx,yy,args,fgg=-1,bgg=-1;
+char *coloursavailable[]={"black","red","green","yellow","blue","magenta","cyan","white"};
+int x,y,xbox,ybox,xx,yy,foregroundcolour=-1,backgroundcolour=-1;
 float outcir,incir,diam,outradi,inradi,r;
 
 
 void textcolor(int attr, int fg, int bg);
+void wrongcolours(int backgroundcolour, int foregroundcolour);
+void colourchecker(char **argvalue);
+
 
 void textcolor(int attr, int fg, int bg)
 {	char command[13];
@@ -41,38 +47,76 @@ void textcolor(int attr, int fg, int bg)
 }
 
 
-void colourchecker(char **argv1){
-	/* this is for checking the background and foreground colour inputs are valid or not
-	*/
-for (args = 0; args < 8; args++)
+
+void wrongcolours(int backgroundcolour, int foregroundcolour){
+	if (backgroundcolour==-1 )
 	{
-		 if(strcmp(argv1[2],colourval[args])==0){
-		 	bgg=args;
-		 }}
-	for (args = 0; args < 8; args++)
-	{	 
-		 if(strcmp(argv1[3],colourval[args])==0){
-		 	fgg=args;
-		 }
-		 	
-	}
-	if (bgg==-1 )
-	{
-	printf("Background color is not available\n");
+		printf("Background color is not available\n");
 		 	exit(0);
-	}else if (fgg==-1)
+	}else if (foregroundcolour==-1)
 	{
 		printf("Foreground color is not available\n");
 		 	exit(0);
 	}
+}
+
+
+void colourchecker(char **argvalue){
+	/* this is for checking the background and foreground colour inputs are valid or not
+	*/
+for (argsize = 0; argsize < argsize_max; argsize++)
+	{
+		 if(strcmp(argvalue[2],coloursavailable[argsize])==0){
+		 	backgroundcolour=argsize;
+		 }}
+	for (argsize = 0; argsize < argsize_max; argsize++)
+	{	 
+		 if(strcmp(argvalue[3],coloursavailable[argsize])==0){
+		 	foregroundcolour=argsize;
+		 }
+		 	
+	}
+	void wrongcolours(backgroundcolour,foregroundcolour);
+
+}
+
+void checkerprint(){
+
+
+colourchecker(argvalue);
+		
+		for(y=0;y< box_max	;y++){
+			for(ybox=0;ybox< box_max;ybox++){
+				for(x=0;x<argsize_max;x++){
+					
+					for(xbox=0;xbox<box_max	;xbox++){
+						if((x+y)%2==0){
+							textcolor(BRIGHT,backgroundcolour,foregroundcolour);	//changing colours
+							printf(" ");
+
+						}else{
+							textcolor(BRIGHT,foregroundcolour,backgroundcolour);	
+							printf(" ");
+							
+
+						}
+						textcolor(BRIGHT,foregroundcolour,backgroundcolour);
+
+					}
+				}
+			printf("\n");
+			}
+
+				
+		}
 
 }
 
 
 
-int main(int argc, char **argv){
+int main(int argc, char **argvalue){
 
-if(argc<4){                            //checking args are less or more
+if(argc<4){                            //checking argsize are less or more
 	printf("Not enough arguments\n");
 	exit(0);
 }
@@ -83,40 +127,15 @@ if(argc>4){
 else{
 
 
-	if(strcmp(argv[1],ck)==0){   //checker printing function
+	if(strcmp(argvalue[1],ck)==0){   //checker printing function
 	
-		colourchecker(argv);
-		
-		for(y=0;y<8;y++){
-			for(ybox=0;ybox<8;ybox++){
-				for(x=0;x<8;x++){
-					
-					for(xbox=0;xbox<8;xbox++){
-						if((x+y)%2==0){
-							textcolor(BRIGHT,bgg,fgg);	//changing colours
-							printf(" ");
-
-						}else{
-							textcolor(BRIGHT,fgg,bgg);	
-							printf(" ");
-							
-
-						}
-						textcolor(BRIGHT,fgg,bgg);
-
-					}
-				}
-			printf("\n");
-			}
-
-				
-		}
+		checkerprint();
 
 	}
 
-	else if (strcmp(argv[1],dn)==0){          //funtion for donut printer
+	else if (strcmp(argvalue[1],dn)==0){          //funtion for donut printer
 		
-	colourchecker(argv);
+	colourchecker(argvalue);
 		scanf("%f",&diam);
 
 		r=diam/2;
@@ -126,22 +145,22 @@ else{
 			outradi=((x-r)*(x-r)+(y-r)*(y-r));
 			
 			if((outradi)<(r*r) && outradi>(r/2)*(r/2)){
-				textcolor(BRIGHT,bgg,fgg);	          //printing big circle
+				textcolor(BRIGHT,backgroundcolour,foregroundcolour);	          //printing big circle
 				printf(" ");
-				textcolor(BRIGHT,fgg,bgg);	
+				textcolor(BRIGHT,foregroundcolour,backgroundcolour);	
 				
 			 
 
 			}
 			
 			else if((outradi)<=((r/2)*(r/2))){         //removing small circle from big one
-				textcolor(BRIGHT,fgg,bgg);	
+				textcolor(BRIGHT,foregroundcolour,backgroundcolour);	
 				printf(" ");
                                         
 			}
 			else{
 				
-				textcolor(BRIGHT,fgg,bgg);	   //printing outer
+				textcolor(BRIGHT,foregroundcolour,backgroundcolour);	   //printing outer
 				printf(" ");
 				
 				
