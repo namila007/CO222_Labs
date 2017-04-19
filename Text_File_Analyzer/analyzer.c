96f731521ac31023ca0d55380c4167f8286b110c
Namila Bandara <namila007@gmail.com>
2017-04-19 21:47:53 +0530

    moved to new repo
    https://github.com/namila007/Text-File-Analyzer

--- Text_File_Analyzer/analyzer.c
+++ Text_File_Analyzer/analyzer.c
@@ -5,22 +5,127 @@ E/14/084
 */
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define READ "r"
+#define SIZE 100
+
+
+
 
 typedef struct node{
 	//creating a linked list for store data
 	char* string;
 	int count;
-	struct node *next
+	struct node *next;
 }	linklist;  //linked list name
+
+
+char* removeNonAlphaNum(char *str);
+linklist* find(char* string);
+void insertdata(char* string);
+void readfile(FILE *fp,char *filepath);
+int nodelength();
+void sortnodes();
+
 
 linklist *head=NULL;   //making head and current null
 linklist *current=NULL;
 
+int totalcount=0;
 
-linklist find(char* string){
-	linklist current=head;
+int nodelength(){
+	int length=0;;
+	linklist * current;
+	current=head;
+
+	for(current=head;current!=NULL;current=current->next){
+		length++;
+	}
+
+	return length;
+}
+
+
+void printList() {
+   linklist *ptr = head;
+   printf("\n[ ");
+	
+   //start from the beginning
+   while(ptr != NULL) {
+      printf("%s,%d ",ptr->string,ptr->count);
+      double t=ptr->count;
+   
+      printf("average:%.02lf\n",(double)(t/totalcount)*100);
+      ptr = ptr->next;
+   }
+	
+   printf(" ]");
+}
+
+void sortnodes(){
+	linklist *current, *nextnode;
+	int i,j,k,nodesize,count;
+	char *tempstring;
+
+	nodesize=nodelength();
+	k=nodesize;
+
+	for(i=0;i<nodesize;i++,k--){
+		current=head;
+		nextnode=head->next;
+
+		for(j=1;j<k;j++){
+			if(nextnode->count>current->count){
+				int strlength=strlen(nextnode->string);
+				tempstring=(char*)malloc((strlength+1)*sizeof(char));
+				
+
+				strcpy(tempstring,nextnode->string);
+				count=nextnode->count;
+
+				nextnode->string=realloc(nextnode->string,strlen(current->string)+1);
+				strcpy(nextnode->string,current->string);
+				nextnode->count=current->count;
+
+				current->string=realloc(current->string,strlen(tempstring)+1);
+				strcpy(current->string,tempstring);
+				current->count=count;
+			}
+			current = current->next;
+         	nextnode = nextnode->next;
+
+		}
+	}
+
+
+}
+
+
+char* removeNonAlphaNum(char *str)
+{
+    int i = 0, j = 0;
+    int arraysize=strlen(str)+1;
+    char c;
+    char *temp=malloc(arraysize* sizeof(char));
+
+    for(i=0;i<arraysize;i++){
+        c=str[i];
+        if(isalnum(c)!= 0){
+            temp[j++]=c;
+        }
+        
+    }
+    temp[j]='\0';
+
+
+    
+//    return str;
+    return temp;
+}
+
+linklist* find(char* string){
+	 current=head;
 
 	if(head==NULL){
 		return NULL;
@@ -40,17 +145,20 @@ linklist find(char* string){
 
 
 void insertdata(char* string){
-	linklist *link=(linklist)malloc (sizeof(linklist));
-	current=find(string);
+	char *temp=removeNonAlphaNum(string);
+	current=find(temp);
 	
 	if(current==NULL){
-		link->string=string;
+		linklist *link=(linklist*)malloc (sizeof(link));
+
+		link->string=temp;
 		link->count=1;
+		totalcount=1+totalcount;
 		link->next=head;
 		head=link;
 	}else{
 		current->count=(current->count)+1;
-		
+		totalcount=1+totalcount;
 
 	}
 
@@ -62,10 +170,17 @@ void insertdata(char* string){
 
 
 
-void readfile(File *fp,char *filepath){
-
+void readfile(FILE *fp,char *filepath){
+	char string[SIZE];
+	
+	
 	fp=fopen(filepath ,READ);
 
+	while(fscanf(fp,"%s",string)!=EOF){
+		char *data=malloc(sizeof(strlen(string)));
+		strcpy(data,string);
+		insertdata(data);
+	}
 
 
 }
@@ -75,11 +190,13 @@ void readfile(File *fp,char *filepath){
 int main(int argc, char **argv){
 
 	char *filepath=argv[1];
-	File *fp;
+	FILE *fp;
 
-
-
-
+	readfile(fp,filepath);
+	//printList();
+	sortnodes();
+	printList();
+	printf("%d\n",totalcount );
 
 
 }
